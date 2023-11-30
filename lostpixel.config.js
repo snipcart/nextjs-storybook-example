7494e930e0155da52e2d09ce9b94b6a3cc2038ac
JDaug1326 <jaymesonheller@gmail.com>
2023-11-30 10:29:59 +0000

    setting up lost pixel

---
+++ lostpixel.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  pageShots: {
+    pages: [{ path: "/", name: "landing" }],
+    baseUrl: "http://172.17.0.1:3000",
+  },
+  lostPixelProjectId: "clpl1p7ar3htsek0e1tlmcvmd",
+  apiKey: process.env.LOST_PIXEL_API_KEY,
+};
